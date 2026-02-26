1feb53e75cdea94e37a09bc3bf85468a6b3b6c34
seungrokj <seungrok.jung@amd.com>
2026-02-26 01:54:49 +0000

    update

--- src/srtctl/backends/trtllm.py
+++ src/srtctl/backends/trtllm.py
@@ -218,7 +218,8 @@ class TRTLLMProtocol:
             ]
 
         #if process.endpoint_mode != "prefill" and index == 0:
-        if process.endpoint_mode != "prefill":
+        #if process.endpoint_mode != "prefill":
+        if 1:
             nsys_path = f"/logs/{process.endpoint_mode}_nsys_{index}_report%p"
             cmd = [
                 "nsys",
@@ -231,6 +232,7 @@ class TRTLLMProtocol:
                 "--trace=cuda,nvtx",
                 "--cuda-memory-usage=true",
                 "--delay=300",
+                "--flush-on-cudaprofilerstop=false",
                 "trtllm-llmapi-launch",
                 "python3",
                 "-m",
